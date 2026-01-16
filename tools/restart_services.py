c3fbc6df5bf02c8a7fa6237318933310d69d965f
Ryan Schostag <ryanschostag@gmail.com>
2026-01-15 17:00:35 -0800

    Fix: Rebuild the services, which helps after applying code changes

--- tools/restart_services.py
+++ tools/restart_services.py
@@ -6,29 +6,20 @@ from typing import List, Optional
 from utils import run_commands
 
 
-def restart_container_services(services : Optional[List[str]] = None) -> bool:
+def restart_container_services() -> bool:
     """
     Runs docker compose up on the container_names outlined in docker-compose.yml
     """
-    commands = []
-    if services is None:
-        commands.extend([
-            'docker compose --profile cpu down',
-            'docker compose --profile cpu up -d',
-            'docker compose logs -f --tail=50 llm'
-        ])
-    else:
-        services = ' '.join(services)
-        commands.extend([
-            f'docker compose --profile cpu down {services}',
-            f'docker compose --profile cpu up -d {services}',
-            f'docker compose logs -f --tail=200'
-        ])
+    commands = [
+        'docker compose --profile cpu down',
+        'docker compose --profile cpu build --no-cache',
+        'docker compose --profile cpu up -d',
+        # 'docker compose logs -f --tail=50 llm'
+    ]
     return run_commands(commands)
 
 
 if __name__ == "__main__":
-    services_to_restart = None
-    return_code = 0 if restart_container_services(services=services_to_restart) else 1
+    return_code = 0 if restart_container_services() else 1
     sys.exit(return_code)
 
