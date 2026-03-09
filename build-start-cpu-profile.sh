dea201bf512c58993493699e7380831ebe7ec501
Ryan Schostag <ryanschostag@gmail.com>
2026-03-08 19:28:58 -0700

    (fix)
    - to help with troubleshooting during the build scripts, added tee to
      output files

--- build-start-cpu-profile.sh
+++ build-start-cpu-profile.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+set -euo pipefail
 
 
-docker compose --profile test down
+down_file="down.log"
+build_file="build.log"
+up_file="up.log"
+
+
+time docker compose --profile test down 2>&1 | tee $down_file
 sleep 1
-docker compose --profile cpu build --no-cache
+time docker compose --profile cpu build --no-cache 2>&1 | tee $build_file
 sleep 1
-docker compose --profile cpu up -d
+time docker compose --profile cpu up -d 2>&1 | tee $up_file
+
 echo Complete!
 
